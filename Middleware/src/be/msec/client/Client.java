a76f4d4149de06ebc76a69902c556c4d5148dfc5
TimL <timleenaers@gmail.com>
2017-05-29 11:23:38 +0200

    Comments how APDU works

--- Middleware/src/be/msec/client/Client.java
+++ Middleware/src/be/msec/client/Client.java
@@ -78,9 +78,34 @@ public class Client {
 			a = new CommandAPDU(InstructionCodes.IDENTITY_CARD_CLA, InstructionCodes.DO_HELLO_INS, 0x00, 0x00, hello_current,0x7f);
 			r = c.transmit(a);		
 			
+			/*
+			 * r (Response) contains
+			 * - Data field = max 255 bytes at a time 
+			 * - SW1 = feedback code
+			 * - SW2 = feedback code
+			 * 
+			 * Data (r.getData()) field contains
+			 * - All fields of CommandAPDU + length_of_response + response
+			 * So:
+			 * - CLA = 1 byte
+			 * - INS = 1 byte
+			 * - P1 = 1 byte
+			 * - P2 = 1 byte
+			 * - Lc = 1 byte
+			 * - data_send_to_card
+			 * - length_of_response
+			 * - response
+			 * 
+			 * EXAMPLE:
+			 * - bytes send to: 0x48 0x65 0x6c 0x6c 0x6f 0x00 0x00 0x01 0x5c 0x53 0x7a 0xd9 0x66 
+			 * - response_card: 0x80 0x28 0x00 0x00 0x0d 0x48 0x65 0x6c 0x6c 0x6f 0x00 0x00 0x01 0x5c 0x53 0x7a 0xd9 0x66  0x01     0x01 
+			 * 					CLA	|INS | P1 | P2 |Lc   |                   bytes send to card                         | length | response
+			 */
+			
 			if (r.getSW()!=0x9000) throw new Exception("Sending current time failed");
 			
 			System.out.println("Checking if revalidation request is needed");
+
 			short result = r.getData()[hello_current.length+6];	
 			if(result==0x01)
 			{
